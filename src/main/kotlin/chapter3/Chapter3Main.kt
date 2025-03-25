55b2b1c488fa37418e462dead98ed117522a0ef3
cjcjon <cjcjon@naver.com>
2025-03-25 21:50:22 +0900

    feat: chapter3 일부 구현

---
+++ src/main/kotlin/chapter3/Chapter3Main.kt
@@ -0,0 +1,43 @@
+package org.example.chapter3
+
+sealed class List<out A> {
+    data object Nil: List<Nothing>()
+    data class Cons<out A>(val head: A, val tail: List<A>): List<A>()
+
+    companion object {
+        fun <A> of(vararg args: A): List<A> {
+            val tail = args.sliceArray(1 until args.size)
+            return if (args.isEmpty()) Nil else Cons(args[0], of(*tail))
+        }
+
+        fun sum(ints: List<Int>): Int =
+            when (ints) {
+                is Nil -> 0
+                is Cons -> ints.head + sum(ints.tail)
+            }
+
+        fun product(doubles: List<Double>): Double =
+            when (doubles) {
+                is Nil -> 1.0
+                is Cons ->
+                    if (doubles.head == 0.0) 0.0
+                    else doubles.head * product(doubles.tail)
+            }
+
+        /* 연습문제 3-1 */
+        fun <A> tail(xs: List<A>): List<A> = when (xs) {
+            is Nil -> Nil
+            is Cons -> xs.tail
+        }
+
+        /* 연습문제 3-2 */
+        fun <A> setHead(xs: List<A>, x: A): List<A> = when (xs) {
+            is Nil -> Cons(x, Nil)
+            is Cons -> Cons(x, xs.tail)
+        }
+    }
+}
+
+fun main() {
+
+}
