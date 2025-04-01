6d60c8927948754347189a883c1ef461e02e42d4
cjcjon <cjcjon@naver.com>
2025-04-02 00:08:26 +0900

    feat(chapter4): 1번 구현

--- src/main/kotlin/chapter4/Chapter4Main.kt
+++ src/main/kotlin/chapter4/Chapter4Main.kt
@@ -5,6 +5,34 @@ sealed class Option<out A> {
     data object None : Option<Nothing>()
 }
 
+/* 연습문제 4-1 */
+fun <A, B> Option<A>.map(f: (A) -> B): Option<B> = when (this) {
+    is Option.None -> this
+    is Option.Some -> Option.Some(f(this.get))
+}
+
+fun <A, B> Option<A>.flatMap(f: (A) -> Option<B>): Option<B> = when (this) {
+    is Option.None -> Option.None
+    is Option.Some -> f(this.get)
+}
+
+fun <A> Option<A>.getOrElse(default: () -> A): A = when (this) {
+    is Option.None -> default()
+    is Option.Some -> this.get
+}
+
+fun <A> Option<A>.orElse(ob: () -> Option<A>) = when (this) {
+    is Option.None -> ob()
+    is Option.Some -> this
+}
+
+fun <A> Option<A>.filter(f: (A) -> Boolean): Option<A> = when (this) {
+    is Option.None -> this
+    is Option.Some ->
+        if (f(this.get)) this
+        else Option.None
+}
+
 fun main() {
 
 }
