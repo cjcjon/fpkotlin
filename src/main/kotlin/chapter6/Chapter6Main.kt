4d20adfd994db2259b4576ece75a7278432ecefd
cjcjon <cjcjon@naver.com>
2025-04-15 23:05:09 +0900

    feat(chapter6): unit, map 구현

--- src/main/kotlin/chapter6/Chapter6Main.kt
+++ src/main/kotlin/chapter6/Chapter6Main.kt
@@ -67,3 +67,15 @@ fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> {
 
     return go(count, List.empty<Int>() to rng)
 }
+
+typealias Rand<A> = (RNG) -> Pair<A, RNG>
+
+fun <A> unit(a: A): Rand<A> = { rng -> a to rng }
+
+fun <A, B> map(s: Rand<A>, f: (A) -> B): Rand<B> = { rng ->
+    val (a, rng2) = s(rng)
+    f(a) to rng2
+}
+
+fun nonNegativeEven(): Rand<Int> =
+    map(::nonNegativeInt) { it - (it % 2) }
