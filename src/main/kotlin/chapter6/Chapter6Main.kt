7ea54b52a1f79174385ff16d80df911225ae3a6a
cjcjon <cjcjon@naver.com>
2025-04-17 22:05:17 +0900

    feat(chapter6): 10번 구현

--- src/main/kotlin/chapter6/Chapter6Main.kt
+++ src/main/kotlin/chapter6/Chapter6Main.kt
@@ -148,3 +148,36 @@ fun <A, B, C> map2(
         f(a, b)
     }
 }
+
+/* 연습문제 6-10 */
+data class State<S, out A>(val run: (S) -> Pair<A, S>) {
+
+    fun <B> flatMap(g: (A) -> State<S, B>): State<S, B> = State { s ->
+        val (value, state) = run(s)
+
+        g(value).run(state)
+    }
+
+    fun <B> map(f: (A) -> B): State<S, B> =
+        flatMap { a -> unit(f(a)) }
+
+    companion object {
+        fun <S, A> unit(a: A): State<S, A> =
+            State { s -> a to s }
+
+        fun <S, A, B, C> map2(
+            sa: State<S, A>,
+            sb: State<S, B>,
+            f: (A, B) -> C,
+        ): State<S, C> = sa.flatMap { a ->
+            sb.map { b ->
+                f(a, b)
+            }
+        }
+
+        fun <S, A> sequence(fs: List<State<S, A>>): State<S, List<A>> =
+            List.foldRight(fs, unit(List.empty())) { f, acc ->
+                map2(f, acc) { h, t -> List.Cons(h, t) }
+            }
+    }
+}
