2436d0f3f6620646d4d2f81edd630d5d67215fb7
cjcjon <cjcjon@naver.com>
2025-04-19 22:27:28 +0900

    feat(chapter7): 3번 구현

--- src/main/kotlin/chapter7/Chapter7Main.kt
+++ src/main/kotlin/chapter7/Chapter7Main.kt
@@ -3,28 +3,76 @@ package org.example.chapter7
 import arrow.core.firstOrNone
 import arrow.core.getOrElse
 import org.example.common.splitAt
+import java.util.concurrent.TimeUnit
 
-class Par<A>(val get: A) {
-    companion object {
-        fun <A> unit(a: A): Par<A> = Par(a)
-        fun <A> lazyUnit(a: () -> A): Par<A> = Par(a())
-        fun <A> run(a: Par<A>): A = a.get
-        fun <A> fork(f: () -> Par<A>): Par<A> = f()
-
-        fun <A, B, C> map2(px: Par<A>, py: Par<B>, f: (A, B) -> C): Par<C> =
-            Par(f(px.get, py.get))
-    }
+fun interface Callable<A> {
+    fun call(): A
 }
 
-fun sum(ints: List<Int>): Par<Int> =
-    if (ints.size <= 1)
-        Par.unit(ints.firstOrNone().getOrElse { 0 })
-    else {
-        val (l, r) = ints.splitAt(ints.size / 2)
-        Par.map2(
-            Par.fork { sum(l) },
-            Par.fork { sum(r) }
-        ) { lx: Int, rx: Int ->
-            lx + rx
-        }
+interface Future<A> {
+    fun get(): A
+    fun get(timeout: Long, timeUnit: TimeUnit): A
+    fun cancel(evenIfRunning: Boolean): Boolean
+    fun isDone(): Boolean
+    fun isCancelled(): Boolean
+}
+
+interface ExecutorService {
+    fun <A> submit(c: Callable<A>): Future<A>
+}
+
+typealias Par<A> = (ExecutorService) -> Future<A>
+
+object Pars {
+    fun <A> unit(a: A): Par<A> =
+        { es: ExecutorService -> UnitFuture(a) }
+
+    data class UnitFuture<A>(val a: A): Future<A> {
+        override fun get(): A = a
+        override fun get(timeout: Long, timeUnit: TimeUnit): A = a
+        override fun cancel(evenIfRunning: Boolean): Boolean = false
+        override fun isDone(): Boolean = true
+        override fun isCancelled(): Boolean = false
     }
+
+    data class TimedMap2Future<A, B, C>(
+        val pa: Future<A>,
+        val pb: Future<B>,
+        val f: (A, B) -> C
+    ) : Future<C> {
+        override fun isDone(): Boolean = TODO("Unused")
+        override fun get(): C = TODO("Unused")
+        override fun get(timeout: Long, timeUnit: TimeUnit): C {
+            val timeoutMillis = TimeUnit.MILLISECONDS.convert(timeout, timeUnit)
+
+            val start = System.currentTimeMillis()
+            val a = pa.get(timeout, timeUnit)
+            val duration = System.currentTimeMillis() - start
+
+            val remainder = timeoutMillis - duration
+            val b = pb.get(remainder, TimeUnit.MILLISECONDS)
+
+            return f(a, b)
+        }
+
+        override fun cancel(evenIfRunning: Boolean): Boolean = TODO("Unused")
+
+        override fun isCancelled(): Boolean = TODO("Unused")
+    }
+
+    fun <A, B, C> map2(
+        a: Par<A>,
+        b: Par<B>,
+        f: (A, B) -> C
+    ): Par<C> = { es: ExecutorService ->
+        val af: Future<A> = a(es)
+        val bf: Future<B> = b(es)
+        UnitFuture(f(af.get(), bf.get()))
+    }
+
+    fun <A> fork(
+        a: () -> Par<A>
+    ): Par<A> = { es: ExecutorService ->
+        es.submit { a()(es).get() }
+    }
+}
