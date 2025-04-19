ee455cde078b3904b67724749b5a32beb99b6f25
cjcjon <cjcjon@naver.com>
2025-04-19 23:22:34 +0900

    feat(chapter7): 12번 구현

--- src/main/kotlin/chapter7/Chapter7Main.kt
+++ src/main/kotlin/chapter7/Chapter7Main.kt
@@ -118,21 +118,37 @@ object Pars {
     }
 
     /* 연습문제 7-10 */
-    fun <A> choiceN(n: Par<Int>, choices: List<Par<A>>): Par<A> = { es: ExecutorService ->
-        choices[n(es).get()].invoke(es)
-    }
+//    fun <A> choiceN(n: Par<Int>, choices: List<Par<A>>): Par<A> = { es: ExecutorService ->
+//        choices[n(es).get()].invoke(es)
+//    }
 
-    fun <A> choice(cond: Par<Boolean>, t: Par<A>, f: Par<A>): Par<A> = { es: ExecutorService ->
-        choiceN(map(cond, { if (it) 1 else 0 }), listOf(f, t))(es)
-    }
+//    fun <A> choice(cond: Par<Boolean>, t: Par<A>, f: Par<A>): Par<A> = { es: ExecutorService ->
+//        choiceN(map(cond, { if (it) 1 else 0 }), listOf(f, t))(es)
+//    }
 
     /* 연습문제 7-11 */
+//    fun <K, V> choiceMap(
+//        key: Par<K>,
+//        choices: Map<K, Par<V>>
+//    ): Par<V> = { es: ExecutorService ->
+//        choices[key(es).get()]!!.invoke(es)
+//    }
+
+    /* 연습문제 7-12 */
+    fun <A, B> chooser(pa: Par<A>, choices: (A) -> Par<B>): Par<B> = { es: ExecutorService ->
+        choices(pa(es).get()).invoke(es)
+    }
+
+    fun <A> choiceN(n: Par<Int>, choices: List<Par<A>>): Par<A> =
+        chooser(n) { idx -> choices[idx] }
+
+    fun <A> choice(cond: Par<Boolean>, t: Par<A>, f: Par<A>): Par<A> =
+        choiceN(map(cond) { if (it) 1 else 0 }, listOf(f, t))
+
     fun <K, V> choiceMap(
         key: Par<K>,
         choices: Map<K, Par<V>>
-    ): Par<V> = { es: ExecutorService ->
-        choices[key(es).get()]!!.invoke(es)
-    }
+    ): Par<V> = chooser(key) { k -> choices[k]!! }
 
     fun sortPar(parList: Par<List<Int>>): Par<List<Int>> =
         map(parList) { it.sorted() }
